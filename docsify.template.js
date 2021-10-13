e15c17de7388c7862f934c184783377ef0b17877
Hector Custódio <hector.custodio@zup.com.br>
2021-10-13 17:05:25 -0300

    Feature/adds theme (#7)
    * Adds beta upload workflow
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Changes c4builder config
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates beta workflow
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Upadtes workflow
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Changes event trigger
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates secrets names
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates workflow trigger
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates workflow name
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Adds theme
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates beta upload action
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates bucket name
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates paths
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates theme
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Uploads paths and actions
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates c4builder
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates en path
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates theme
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates theme
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates release
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Adds beagle logo
    
    Signed-off-by: João Geniselli <joao.geniselli@zup.com.br>
    
    * Updates logo theme
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Updates theme path
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    * Reverts theme path
    
    Signed-off-by: Hector Custódio <hector.custodio@zup.com.br>
    
    Co-authored-by: João Geniselli <joao.geniselli@zup.com.br>

---
+++ docsify.template.js
@@ -0,0 +1,66 @@
+module.exports = options => {
+  return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="UTF-8">
+      <title>Beagle C4 Model</title>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="description" content="Description">
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+      <meta name="description" content="Beagle is...">
+      
+      <!-- OG tags-->
+      <meta property="og:title" content="Beagle" />
+      <meta property="og:url" content="https://docs-beta.usebeagle.io/c4model />
+      <meta property="og:type" content="website"
+      <meta property="og:description" content="Beagle is..." />
+      <meta property="og:locale" content="en-US" />
+      <meta property="og:site_name" content="Beagle" />
+      <meta property="og:titleTemplate" content="Beagle" />
+      <meta property="og:locale:alternate" content="pt-BR" />
+      <!-- OG tags-->
+
+      <link rel="icon" href="https://usebeagle.io/public/icon.png" alt="favicon-32x32"/>
+      
+      <link rel="stylesheet" href="${options.stylesheet}">
+    </head>
+    
+    <body>
+      <div id="app"></div>
+
+      <nav class="language-nav">
+        <div class="language-nav-head">
+          <div class="language-nav-head-text">Português</div>
+
+          <div class="language-nav-head-icon"></div>
+        </div>
+
+        <div class="language-nav-main">
+          <ul class="language-nav-main-list">
+            <li class="language-nav-main-list-item" data-value="c4model/pt-br">Português</li>
+            <li class="language-nav-main-list-item" data-value="c4model/en">English</li>
+          </ul>
+        </div>
+      </nav>
+
+      <script>
+        window.$docsify = ${JSON.stringify(
+          {
+            ...options,
+            el: '#app',
+            logo: 'https://raw.githubusercontent.com/ZupIT/beagle-docs/main/static/shared/logo_beagle_black.svg'
+          },
+          null,
+          2
+        )};
+      </script>
+      <script src="//unpkg.com/docsify/lib/docsify.min.js"></script>
+      <script src="//unpkg.com/docsify-plantuml/dist/docsify-plantuml.min.js"></script>
+      <script src="//cdn.jsdelivr.net/npm/docsify/lib/plugins/zoom-image.min.js"></script>
+      <script src="../theme/theme.js"></script>
+    </body>
+    
+    </html>`
+}
